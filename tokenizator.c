8efcc85f2f6c038a0999e4324c461b123d703212
Raphael Feger <rafeger@bess-f2r3s8.clusters.42paris.fr>
2025-03-23 19:50:09 +0100

    first tokenizator/lexer

--- tokenizator.c
+++ tokenizator.c
@@ -1,21 +1,58 @@
 #include "minishell.h"
+#include <string.h>
 
-
-
-int	is_chara(char *str, int i)
+char	*ft_strndup(char *input, int start, int end)
 {
-	char backslash = '\\';
-	char dollar = '$';
-	char quote = '\'';
-	char double_quote = '\"';
-	while (str[i]);
-}
+	int	i;
 
+	i = 0;
+	char	*word;
+	word = malloc((end - start) * sizeof(char));
+	while(start <= end)
+	{
+		word[i] = input[start];
+		i++;
+		start++;
+	}
+	return (word);
+}
 
 void	add_token(t_token **tokens, t_token_type type, char *value)
 {
-
+	t_token	*new = malloc(sizeof(t_token));
+	if (!new)
+		return;
+	
+new->type = type;
+	new->value = strdup(value); //utiliser ft_strdup
+	new->next = NULL;
+	if (!tokens)
+		*tokens = new;
+	else
+	{
+		t_token *temp = *tokens;
+		while(temp->next)
+			temp = temp->next;
+		temp->next = new;
+	}
 }
+
+int	extract_word(t_token **tokens, char *input, int i)
+{
+	int	j;
+
+	j = i;
+	while (input[i] && input[i] != ' ' && input[i] != 
+		'\t' && input[i] != '>' && input[i] != '<')
+		j++;
+	char *word;
+	word = ft_strndup(input, i, j); //copier de input[i] a input[j]
+	add_token(tokens, WORD, word);
+	free(word);
+
+	return (i);
+}
+
 
 
 // a RACCOURCIR
@@ -63,7 +100,7 @@ t_token	*lexer(char	*input)
 			}
 		}
 		else
-			i += extract_word(&tokens, &input[i]);
+			i += extract_word(&tokens, input, i);
 	}
 	return tokens;
 }
@@ -72,14 +109,15 @@ void	print_tokens(t_token *tokens)
 {
 	while (tokens)
 	{
-		prinf("Token: Type=%d, Value=%s\n", tokens->type, tokens->value);
+		printf("Token: Type=%d, Value=%s\n", tokens->type, tokens->value);
 		tokens = tokens->next;
 	}
 }
 
-int	main(int ac, char **av[])
+int	main(int ac, char *av[])
 {
-	char input = av[1];
+	char *input;
+	input = av[1];
 	t_token *tokens = lexer(input);
 	print_tokens(tokens);
 	return 0;
