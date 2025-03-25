55f72a7f4588b8caaed0487f7ff097affdab826c
Zoe Murie <zmurie@bess-f3r3s3.clusters.42paris.fr>
2025-03-25 11:24:45 +0100

    ouiiiiii

--- builtin/ft_unset.c
+++ builtin/ft_unset.c
@@ -11,7 +11,35 @@
 /* ************************************************************************** */
 #include "../minishell.h"
 
-int	ft_unset(char **str, t_list **env)
+int	check_is_in_env(char *str, t_list *env)
 {
+	t_list	*env_tmp;
 
+	env_tmp = env;
+	while (env_tmp)
+	{
+		if (!ft_strncmp(str[i], env_tmp->str, ft_strlen(str[i])))
+			delete_env_var(pos)
+		env_tmp = env_tmp->next;
+	}
+}
+
+int	ft_unset(char **str, t_list *env)
+{
+	int		i;
+	int		*pos;
+	t_list	*env_tmp;
+
+	i = 0;
+	while (str[i])
+	{
+		env_tmp = env;
+		while (env_tmp)
+		{
+			while (!ft_strncmp(str[i], env_tmp->str, ft_strlen(str[i])))
+				env_tmp = env_tmp->next;
+			
+		} 
+	}
+	return (0);
 }
