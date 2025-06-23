c785e43ef52f40b786469d79131c51ec4125408a
Zoe Murie <zmurie@f4r7s6.clusters.42paris.fr>
2025-06-23 13:07:49 +0200

    yes

--- src/exec-zoe/redirections.c
+++ src/exec-zoe/redirections.c
@@ -12,46 +12,46 @@
 
 #include "../minishell.h"
 
-static void	input_redirection(t_data *data, t_redirect *redir, int *fd)
+static void	input_redirection(t_shell_data *data, t_redirect *redir, int *fd)
 {
-		*fd = open(redir->name, O_RDONLY);
+		*fd = open(redir->file, O_RDONLY);
 	if (*fd == -1)
 		(perror("open"), all_free(data), exit(1));
 	if (*fd != STDIN_FILENO)
 		(dup2(*fd, STDIN_FILENO), close(*fd));
 }
 
-static void	output_redirection(t_data *data, t_redirect *redir, int *fd)
+static void	output_redirection(t_shell_data *data, t_redirect *redir, int *fd)
 {
-	*fd = open(redir->name, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	*fd = open(redir->file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (*fd == -1)
 		(perror("open"), all_free(data), exit(1));
 	if (*fd != STDOUT_FILENO)
 		(dup2(*fd, STDOUT_FILENO), close(*fd));
 }
 
-static void	append(t_data *data, t_redirect *redir, int *fd)
+static void	append(t_shell_data *data, t_redirect *redir, int *fd)
 {
-	*fd = open(redir->name, O_WRONLY | O_CREAT | O_APPEND, 0644);
+	*fd = open(redir->file, O_WRONLY | O_CREAT | O_APPEND, 0644);
 	if (*fd == -1)
 		(perror("open"), all_free(data), exit(1));
 	if (*fd != STDOUT_FILENO)
 		(dup2(*fd, STDOUT_FILENO), close(*fd));
 }
 
-static void	here_document(t_redirect *redir, int *fd)
+static void	here_document(t_redirect *redir, t_cmd *cmd, int *fd)
 {
-	*fd = redir->fd;
+	*fd = cmd->heredoc_fd;
 	if (*fd != STDIN_FILENO)
 		(dup2(*fd, STDIN_FILENO), close(*fd));
 }
 
-void redirections(t_data *data, t_cmd_exec *cmd)
+void redirections(t_shell_data *data, t_cmd *cmd)
 {
-    	t_redir	*redir;
+    t_redirect	*redir;
 	int		fd;
 
-	redir = cmd->redir_first;
+	redir = cmd->redirects;
 	while (redir)
 	{
         if (redir->type == REDIR_IN)
@@ -61,7 +61,7 @@ void redirections(t_data *data, t_cmd_exec *cmd)
 		else if (redir->type == APPEND)
 			append(data, redir, &fd);
 		else if (redir->type == HERE_DOC)
-			here_document(redir, &fd);
+			here_document(redir,cmd, &fd);
 		redir = redir->next;
 	}
 }
