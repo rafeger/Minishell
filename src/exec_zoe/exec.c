ae53fea9e2d42b861b3f5da67a677d165b56aaf1
Zoe Murie <zmurie@f4r13s4.clusters.42paris.fr>
2025-07-02 19:40:42 +0200

    oui

--- src/exec_zoe/exec.c
+++ src/exec_zoe/exec.c
@@ -74,22 +74,31 @@ static int	execute_command_loop(t_shell_data *data)
 static int	handle_single_builtin(t_shell_data *data, t_cmd *cmd)
 {
 	int	fd;
+	t_redirect	*redir;
 
 	fd = 0;
-	if (!cmd->args && cmd->redirects)
+	redir = cmd->redirects;
+	if (!cmd->args && redir)
 	{
-		if (cmd->redirects->type == APPEND)
-			fd = open(cmd->redirects->file, O_WRONLY | O_CREAT | O_APPEND, 0644);
-		if (cmd->redirects->type == REDIR_OUT)
-			fd = open(cmd->redirects->file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-		if (fd == -1)
+		while (redir)
 		{
-			ft_cleanup_shell(&data);
-			rl_clear_history();
-			perror("open");
-			exit(1);
+			if (redir->type == APPEND)
+				fd = open(redir->file, O_WRONLY | O_CREAT | O_APPEND, 0644);
+			if (redir->type == REDIR_OUT)
+				fd = open(redir->file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+			if (redir->type == REDIR_IN)
+				fd = open(redir->file, O_RDONLY);
+			if (fd == -1)
+			{
+				ft_cleanup_shell(&data);
+				rl_clear_history();
+				ft_putstr_fd("bash: ", 2);
+				//ft_putstr_fd(redir->file, 2);
+				perror("");
+			}
+			close(fd);
+			redir = redir->next;
 		}
-		close(fd);
 		close_heredoc_fds(data->cmd);
 		data->last_exit_status = 0;
 		return (1);
