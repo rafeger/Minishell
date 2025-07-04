27e1554aef364bdaaace9266e1b36d8dc927c7b1
Zoe Murie <zmurie@f4r14s6.clusters.42paris.fr>
2025-07-04 18:20:10 +0200

    oui

--- src/exec_zoe/find_cmd.c
+++ src/exec_zoe/find_cmd.c
@@ -15,7 +15,7 @@ static void	print_command_not_found(char *cmd)
 {
 	ft_putstr_fd("bash: ", 2);
 	ft_putstr_fd(cmd, 2);
-	perror(" ");
+	ft_putstr_fd(": command not found\n", 2);
 }
 
 static void	cleanup_split(char **array)
