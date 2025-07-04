aeb15da72931f0d945016b6a4233e066ebd4ac1c
Raph <raphael.feger@proton.me>
2025-07-04 09:54:20 +0200

    yes

---
+++ src/lexer/lexer_quote.c
@@ -0,0 +1,42 @@
+#include "../../include/minishell.h"
+
+void	quote_state_machine_new(t_ta *lx, char c)
+{
+    int	was_quoted;
+
+    if (is_quote_char(c))
+    {
+        if (!lx->in_q)
+        {
+            lx->in_q = 1;
+            lx->quotechar = c;
+            lx->second_quote = 1;
+        }
+        else if (c == lx->quotechar)
+        {
+            was_quoted = lx->in_q;
+            lx->in_q = 0;
+            lx->quotechar = '\0';
+            lx->second_quote = was_quoted;
+        }
+        else
+            lx->token[lx->tokenindex++] = c;
+    }
+    else
+        lx->token[lx->tokenindex++] = c;
+}
+
+void	handle_quotes_new(t_ta *lx, char **input)
+{
+    if ((!lx->in_q) && ((**input == '"' && *(*input + 1) == '"') ||
+            (**input == '\'' && *(*input + 1) == '\'')))
+    {
+        if (*(*input + 2) && ft_isspace(*(*input + 2)))
+            lx->trailing_space = 1;
+        *input += 1;
+        return ;
+    }
+    quote_state_machine_new(lx, **input);
+    if (!lx->in_q && *(*input + 1) && ft_isspace(*(*input + 1)))
+        lx->trailing_space = 1;
+}
